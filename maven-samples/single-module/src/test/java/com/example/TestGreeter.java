a18a7539ed69085c36c5024d5e25f500800d86d4
Sharath Babu <sharath.ise@gmail.com>
2023-01-27 22:30:00 +0530

    My first Maven changes

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,6 +37,8 @@ public class TestGreeter {
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
+      assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 
       @Test
       public void greetShouldIncludeGreetingMessage_New() {
